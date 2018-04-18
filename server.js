9db3c29d9a359f4140436cc50437f4ac17d37828
vanhooktristan <vanhooktristan@gmail.com>
2018-04-17 21:07:32 -0500

    second commit

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ var characters = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/add", function(req, res) {
